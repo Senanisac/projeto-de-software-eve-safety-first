389d945ee08c4c908d1558fb70344db075ca065b
Senanisac <isac.djenonlo@ctec.ufal.br>
2026-03-25 20:26:37 -0300

    Adicionando classe Avaliacao - Funcionalidade 8

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,1 +1,20 @@
- 
+"""
+Classe Avaliacao - Funcionalidade 8: Sistema de avaliação
+"""
+
+from datetime import datetime
+import uuid
+
+
+class Avaliacao:
+    def __init__(self, corrida_id: str, avaliador_id: str, avaliado_id: str, nota: int, comentario: str = ""):
+        self.id = str(uuid.uuid4())
+        self.corrida_id = corrida_id
+        self.avaliador_id = avaliador_id
+        self.avaliado_id = avaliado_id
+        self.nota = nota  # 1 a 5
+        self.comentario = comentario
+        self.data_hora = datetime.now()
+    
+    def __str__(self) -> str:
+        return f"Avaliação: {self.nota}/5 - {self.comentario}"
